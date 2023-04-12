187e4fd26ecd07dbcb15e990854f3440bb5bda74
MononoMorapeli <minkymorapeli@gmail.com>
2023-04-12 18:32:55 +0200

    Functions in Javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const poundToKg =(pound) =>{
+    return pound * 0,453592;
+}
+console.log(poundToKg(10));
+
+
+
+const inchesToCentimeters = (inches) =>{
+    return inches * 2.54;
+}
+console.log(inchesToCentimeters(10));
+
+const fahToCelsius =(fah)=>{
+    return fah * -17.2222;
+}
+console.log(fahToCelsius(10))
+const feetToMeter =(feet)=>{
+    return feet * 0.3048;
+}
+console.log(feetToMeter(10));
+
+3
